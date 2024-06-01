c2d34ebc370de64c0735216464cebb6f8422d3c8
Sanket Kapile <sanketkapile1090@gmail.com>
2024-06-01 10:43:31 +0100

    Made changes in DB connection and Class Connection

---
+++ src/com/group2/ecom/DatabaseConnection.java
@@ -0,0 +1,29 @@
+package com.group2.ecom;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class DatabaseConnection {
+	private static final String DBTYPE = "jdbc:mysql";
+	private static final String DATABASENAME = "ECOM";
+	private static final String HOSTADDRESS = "://localhost:3306/";
+	private static final String DRIVERNAME = "com.mysql.cj.jdbc.Driver";
+	private static final String DBNAME = DBTYPE + HOSTADDRESS + DATABASENAME;
+	private static final String USERNAME = "root";
+	private static final String PASSWORD = "root";
+	public static Connection con;
+	public static String query;
+	public static PreparedStatement pStmt;
+	public static ResultSet rs;
+	public void dbConnect() {
+		try {
+			Class.forName(DRIVERNAME);
+			con = DriverManager.getConnection(DBNAME, USERNAME, PASSWORD);
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
